667acff40fff345f3629b0b6c6798af6b3ac42a0
Swastik369 <swastikpari123@gmail.com>
2024-05-13 19:15:42 +0000

    vicky: LineageOS compatible device tree
    Signed-off-by: Swastik369 <swastikpari123@gmail.com>

---
+++ README.md
@@ -0,0 +1,9 @@
+# Android device tree for motorola moto g72 (vicky)
+
+```
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ lineage_vicky.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from vicky device
+$(call inherit-product, device/motorola/vicky/device.mk)
+
+PRODUCT_DEVICE := vicky
+PRODUCT_NAME := lineage_vicky
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := moto g72
+PRODUCT_MANUFACTURER := motorola
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vicky_g_sys-user 13 T2SVS33M.68-21-6-3 e8924 release-keys"
+
+BUILD_FINGERPRINT := motorola/vicky_g_sys/vicky:13/T2SVS33M.68-21-6-3/e8924:user/release-keys
